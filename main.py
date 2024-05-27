856ff607c043a0ffbc621dacced532612e3c8fc8
AndreSVasconcelos <andres.vasconcelos@gmail.com>
2024-05-27 15:28:12 -0300

    atualizações main

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ grafo = Grafo()
 grafo.arad.mostra_adjacentes()
 
 # Busca Gulosa
-vetor = VetorOrdenado(5)
+""" vetor = VetorOrdenado(5)
 vetor.insere(grafo.arad)
 vetor.insere(grafo.craiova)
 vetor.insere(grafo.bucharest)
@@ -16,7 +16,7 @@ vetor.insere(grafo.lugoj)
 vetor.imprime()
 
 busca_gulosa = Gulosa(grafo.bucharest)
-busca_gulosa.buscar(grafo.arad)
+busca_gulosa.buscar(grafo.arad) """
 
 # Busca A Estrela
 vetorAEstrela = VetorOrdenadoAEstrela(3)
