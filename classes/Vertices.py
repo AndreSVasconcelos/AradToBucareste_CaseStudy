02717d13e4512932771229675d4e89ddb5bf39a5
AndreSVasconcelos <andres.vasconcelos@gmail.com>
2024-05-27 11:41:00 -0300

    Correcoes imports

--- classes/Vertices.py
+++ classes/Vertices.py
@@ -1,12 +1,14 @@
 class Vertice:
-    def __init__(self, rotulo):
-        self.rotulo = rotulo
-        self.visitado = False
-        self.adjacentes = []
-    
-    def add_adjacentes(self, adjacente):
-        self.adjacentes.append(adjacente)
-    
-    def mostra_adjacentes(self):
-        for i in self.adjacentes:
-            print(i.vertice.rotulo, i.custo)
+
+  def __init__(self, rotulo, distancia_objetivo):
+    self.rotulo = rotulo
+    self.visitado = False
+    self.distancia_objetivo = distancia_objetivo
+    self.adjacentes = []
+
+  def adiciona_adjacente(self, adjacente):
+    self.adjacentes.append(adjacente)
+
+  def mostra_adjacentes(self):
+    for i in self.adjacentes:
+      print(i.vertice.rotulo, i.custo)
---
+++ main.py
@@ -0,0 +1,3 @@
+from classes.Grafos import Grafo
+
+Grafo().arad.mostra_adjacentes()
