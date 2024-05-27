04dbf6017852966cadd7cc3ccad9f5c5259ae2d5
AndreSVasconcelos <andres.vasconcelos@gmail.com>
2024-05-27 12:14:14 -0300

    Começando a implementar busca

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from classes.Grafos import Grafo
+from classes.VetoresOrdenados import VetorOrdenado
 
-Grafo().arad.mostra_adjacentes()
+grafo = Grafo()
+grafo.arad.mostra_adjacentes()
+
+vetor = VetorOrdenado(5)
+vetor.insere(grafo.arad)
+vetor.insere(grafo.craiova)
+vetor.insere(grafo.bucharest)
+vetor.insere(grafo.dobreta)
+vetor.imprime()
+
+vetor.insere(grafo.lugoj)
+vetor.imprime()
